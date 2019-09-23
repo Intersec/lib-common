0837ebe6d4014447d44d0a501e7b3406aa5121b1
Arthur Bombarde <arthur.bombarde@intersec.com>
2019-09-23 11:32:07 +0200

    ssl/tls: factorize initialization of SSL_CTX
    All code using TLS now initialize SSL_CTX with the same function.
    A verification is added to check the consistency of the provided private
    key / certificate if they are provided.
    
    Change-Id: I8ff7f790ed2f9171247827534b3d313589366711
    
    rip-it: 2d258cda6caa84f48151b3c05b6cfb4b463d88e2

--- ssl.h
+++ ssl.h
@@ -90,6 +90,7 @@
  *
  */
 
+#include <openssl/ssl.h>
 #include <openssl/evp.h>
 #include <openssl/aes.h>
 #include <openssl/bio.h>
@@ -483,25 +484,21 @@ int rsa_verif_finish(rsa_verif_t * nonnull * nonnull ctx);
  */
 X509 *ssl_ctx_convert_certificate_lstr(lstr_t cert);
 
-/** Load a certificate into the SSL_CTX.
- *
- * A wrapper of SSL_CTX_use_certificate_file for lstr.
- *
- * \param[in]  ctx  The SSL_CTX to enrich.
- * \param[in]  cert  The certificate in PEM format.
- * \return 0 on success and -1 on error.
- */
-int ssl_ctx_use_certificate_lstr(SSL_CTX *ctx, lstr_t cert);
+typedef int (*SSL_verify_cb)(int preverify_ok, X509_STORE_CTX *x509_ctx);
 
-/** Load a private key into the SSL_CTX.
+/** Generic helper to create a TLS context.
  *
- * A wrapper of SSL_CTX_use_PrivateKey for lstr.
- *
- * \param[in]  ctx  The SSL_CTX to enrich.
- * \param[in]  key  The private key in PEM format.
- * \return 0 on success and -1 on error.
+ * \param[in]  meth        TLS method: TLS_server_method(),
+ *                         TLS_client_method() or TLS_method() for both.
+ * \param[in]  key         The TLS private key (optional).
+ * \param[in]  cert        The TLS certificate (optional).
+ * \param[in]  verify_mode The TLS verify mode (cf man SSL_CTX_set_verify).
+ * \param[in]  verify_cb   The TLS verify_cb (cf man SSL_CTX_set_verify).
+ * \param[out] err         The sb that describes the error if an error occured.
+ * \return a pointer on a SSL_CTX on success, NULL on error
  */
-int ssl_ctx_use_privatekey_lstr(SSL_CTX *ctx, lstr_t key);
+SSL_CTX *ssl_ctx_new_tls(const SSL_METHOD *meth, lstr_t key, lstr_t cert,
+                         int verify_mode, SSL_verify_cb verify_cb, sb_t *err);
 
 typedef enum ssl_handshake_status_t {
     SSL_HANDSHAKE_SUCCESS,
