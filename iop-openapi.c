5d5f6b9cd1c0fd415ed12c97d011c6ada5b32edb
Vincent Thiberville <vincent.thiberville@intersec.com>
2019-11-08 15:33:59 +0100

    iop-openapi: add schema for primitive iop types
    Add names for the iop primitives types, so that they can be easily
    referred in more complex types.
    Unfortunately, OpenAPI uses the "JSON data schema" which does not seem
    to understand that unsigned number exists. So, there is no valid data
    schema for uint64... It will be typed as "integer", with a format
    "uint64", which is not standard, but widely used.
    
    Change-Id: I2cf7913f857e136b9be0a95bebfa36a7d09fce79
    
    rip-it: bdf0b8efb79f67197c9ca116c87d54ac807066c7

--- iop-openapi.c
+++ iop-openapi.c
@@ -27,6 +27,184 @@
  * for a better understanding of the different objects.
  */
 
+/* {{{ Schema object */
+
+typedef enum openapi_type_t {
+    TYPE_STRING,
+    TYPE_BYTE,
+    TYPE_BOOL,
+    TYPE_DOUBLE,
+
+    TYPE_INT32,
+    TYPE_INT64,
+
+    /* XXX: these are not standard but common format extensions */
+    TYPE_UINT32,
+    TYPE_UINT64,
+} openapi_type_t;
+
+typedef struct schema_object_t {
+    /* Not part of "Schema object" by the spec, but useful to easily create
+     * the name -> object mappings. */
+    lstr_t name;
+
+    openapi_type_t type;
+
+    opt_i64_t minimum; // nullable
+    opt_i64_t maximum; // nullable
+
+    bool _nullable;
+} schema_object_t;
+qvector_t(schema_object, schema_object_t);
+
+static void
+t_schema_object_to_yaml(const schema_object_t * nonnull obj,
+                        yaml_data_t * nonnull out)
+{
+    yaml_data_t data;
+    lstr_t type = LSTR_NULL_V;
+    lstr_t format = LSTR_NULL_V;
+
+    t_yaml_data_new_obj(out, 2);
+    switch (obj->type) {
+      case TYPE_STRING:
+        type = LSTR("string");
+        break;
+
+      case TYPE_BYTE:
+        type = LSTR("string");
+        format = LSTR("byte");
+        break;
+
+      case TYPE_BOOL:
+        type = LSTR("bool");
+        break;
+
+      case TYPE_DOUBLE:
+        type = LSTR("number");
+        format = LSTR("double");
+        break;
+
+      case TYPE_INT32:
+        type = LSTR("integer");
+        format = LSTR("int32");
+        break;
+
+      case TYPE_INT64:
+        type = LSTR("integer");
+        format = LSTR("int64");
+        break;
+
+      case TYPE_UINT32:
+        type = LSTR("integer");
+        format = LSTR("uint32");
+        break;
+
+      case TYPE_UINT64:
+        type = LSTR("integer");
+        format = LSTR("uint64");
+        break;
+    }
+
+    yaml_data_set_string(&data, type);
+    yaml_obj_add_field(out, LSTR("type"), data);
+
+    if (format.s) {
+        yaml_data_set_string(&data, format);
+        yaml_obj_add_field(out, LSTR("format"), data);
+    }
+
+    if (OPT_ISSET(obj->minimum)) {
+        yaml_data_set_int(&data, OPT_VAL(obj->minimum));
+        yaml_obj_add_field(out, LSTR("minimum"), data);
+    }
+
+    if (OPT_ISSET(obj->maximum)) {
+        yaml_data_set_int(&data, OPT_VAL(obj->maximum));
+        yaml_obj_add_field(out, LSTR("maximum"), data);
+    }
+
+    if (obj->_nullable) {
+        yaml_data_set_bool(&data, true);
+        yaml_obj_add_field(out, LSTR("nullable"), data);
+    }
+}
+
+/* }}} */
+/* {{{ Components object */
+
+typedef struct components_object_t {
+    qv_t(schema_object) schemas;
+} components_object_t;
+
+static schema_object_t * nonnull
+add_schema_object(lstr_t name, openapi_type_t type,
+                   qv_t(schema_object) * nonnull schemas)
+{
+    schema_object_t *obj = qv_growlen0(schemas, 1);
+
+    obj->name = name;
+    obj->type = type;
+
+    return obj;
+}
+
+static void add_iop_primitives_schemas(qv_t(schema_object) *schemas)
+{
+    schema_object_t *schema;
+
+#define ADD_INTEGER_SCHEMA(_name, _type, _min, _max)                         \
+    do {                                                                     \
+        schema_object_t *new_schema;                                         \
+                                                                             \
+        new_schema = add_schema_object(LSTR(_name), TYPE_##_type, schemas);  \
+        OPT_SET(new_schema->minimum, (_min));                                \
+        OPT_SET(new_schema->maximum, (_max));                                \
+    } while(0)
+
+    ADD_INTEGER_SCHEMA("iop:i8",  INT32,   INT8_MIN,   INT8_MAX);
+    ADD_INTEGER_SCHEMA("iop:i16", INT32,  INT16_MIN,  INT16_MAX);
+    ADD_INTEGER_SCHEMA("iop:i32", INT32,  INT32_MIN,  INT32_MAX);
+    ADD_INTEGER_SCHEMA("iop:u8",  UINT32,         0,  UINT8_MAX);
+    ADD_INTEGER_SCHEMA("iop:u16", UINT32,         0, UINT16_MAX);
+    ADD_INTEGER_SCHEMA("iop:u32", UINT32,         0, UINT32_MAX);
+    add_schema_object(LSTR("iop:i64"), TYPE_INT64, schemas);
+    schema = add_schema_object(LSTR("iop:u64"), TYPE_UINT64, schemas);
+    OPT_SET(schema->minimum, 0);
+
+    add_schema_object(LSTR("iop:bool"), TYPE_BOOL, schemas);
+    add_schema_object(LSTR("iop:string"), TYPE_STRING, schemas);
+    add_schema_object(LSTR("iop:data"), TYPE_BYTE, schemas);
+    add_schema_object(LSTR("iop:xml"), TYPE_STRING, schemas);
+    schema = add_schema_object(LSTR("iop:void"), TYPE_BOOL, schemas);
+    schema->_nullable = true;
+}
+
+static void t_components_object_init(components_object_t *obj)
+{
+    t_qv_init(&obj->schemas, 0);
+    add_iop_primitives_schemas(&obj->schemas);
+}
+
+static void
+t_components_object_to_yaml(const components_object_t * nonnull obj,
+                                  yaml_data_t * nonnull out)
+{
+    yaml_data_t schemas;
+
+    t_yaml_data_new_obj(out, 1);
+
+    t_yaml_data_new_obj(&schemas, obj->schemas.len);
+    tab_for_each_ptr(schema, &obj->schemas) {
+        yaml_data_t elem;
+
+        t_schema_object_to_yaml(schema, &elem);
+        yaml_obj_add_field(&schemas, schema->name, elem);
+    }
+    yaml_obj_add_field(out, LSTR("schemas"), schemas);
+}
+
+/* }}} */
 /* {{{ Info object */
 
 typedef struct info_object_t {
@@ -55,14 +233,13 @@ static void t_info_object_to_yaml(const info_object_t * nonnull info,
 }
 
 /* }}} */
-/* {{{ IOP OpenAPI objects */
+/* {{{ Public API */
 
 struct iop_openapi_t {
     info_object_t info;
-};
 
-/* }}} */
-/* {{{ Public API */
+    components_object_t components;
+};
 
 iop_openapi_t *t_new_iop_openapi(const lstr_t title, const lstr_t version,
                                  const lstr_t description)
@@ -72,6 +249,8 @@ iop_openapi_t *t_new_iop_openapi(const lstr_t title, const lstr_t version,
     oa->info.title = t_lstr_dup(title);
     oa->info.version = t_lstr_dup(version);
     oa->info.description = t_lstr_dup(description);
+
+    t_components_object_init(&oa->components);
 
     return oa;
 }
@@ -87,6 +266,9 @@ void t_iop_openapi_to_yaml(const iop_openapi_t *openapi, yaml_data_t *out)
 
     t_info_object_to_yaml(&openapi->info, &data);
     yaml_obj_add_field(out, LSTR("info"), data);
+
+    t_components_object_to_yaml(&openapi->components, &data);
+    yaml_obj_add_field(out, LSTR("components"), data);
 }
 
 /* }}} */
@@ -115,16 +297,27 @@ MODULE_END()
 
 /* {{{ Helpers */
 
-static const char *
-t_z_openapi_to_str(const iop_openapi_t *openapi)
+static int
+z_check_yaml(const iop_openapi_t *openapi, const char *filename)
 {
-    t_SB_1k(sb);
+    t_scope;
+    const char *path;
     yaml_data_t data;
+    lstr_t file;
+    SB_1k(sb);
 
     t_iop_openapi_to_yaml(openapi, &data);
     yaml_pack_sb(&data, &sb);
 
-    return sb.data;
+    path = t_fmt("%pL/test-data/openapi/%s", &z_cmddir_g, filename);
+    Z_ASSERT_N(lstr_init_from_file(&file, path, PROT_READ, MAP_SHARED));
+
+    /* remove last newline from file */
+    file.len--;
+
+    Z_ASSERT_LSTREQUAL(file, LSTR_SB_V(&sb));
+
+    Z_HELPER_END;
 }
 
 /* }}} */
@@ -141,28 +334,8 @@ Z_GROUP_EXPORT(iop_openapi)
         t_scope;
         iop_openapi_t *oa;
 
-        oa = t_new_iop_openapi(LSTR("zoomin"), LSTR("1.0.0"), LSTR_NULL_V);
-
-        /* unexpected EOF */
-        Z_ASSERT_STREQUAL(
-            t_z_openapi_to_str(oa),
-            "openapi: 3.0.2\n"
-            "info:\n"
-            "  title: zoomin\n"
-            "  version: 1.0.0"
-        );
-
-        oa = t_new_iop_openapi(LSTR("juice"), LSTR("0.2.3"), LSTR("sheo"));
-
-        /* unexpected EOF */
-        Z_ASSERT_STREQUAL(
-            t_z_openapi_to_str(oa),
-            "openapi: 3.0.2\n"
-            "info:\n"
-            "  title: juice\n"
-            "  version: 0.2.3\n"
-            "  description: sheo"
-        );
+        oa = t_new_iop_openapi(LSTR("zoomin"), LSTR("0.2.3"), LSTR("sheo"));
+        Z_HELPER_RUN(z_check_yaml(oa, "empty.yml"));
     } Z_TEST_END;
 
     /* }}} */
