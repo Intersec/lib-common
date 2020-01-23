de9165ac7848f24e8aaaba8ea04ce47fba7af6c6
Nicolas Pauss <nicolas.pauss@intersec.com>
2020-01-23 16:27:23 +0100

    bench: rewrite iprintf-speed with zbenchmark.
    Change-Id: Idafbca79e096758e0d6de890721a17a835d13c79
    Priv-Id: eebd2b9665f683a82d0241de015d30572a6138e7

--- bench/iprintf-speed.c
+++ bench/iprintf-speed.c
@@ -18,6 +18,7 @@
 
 #include <lib-common/unix.h>
 #include <lib-common/datetime.h>
+#include <lib-common/zbenchmark.h>
 
 typedef struct mcms_event_t {
     int stamp;
@@ -36,86 +37,39 @@ typedef struct mcms_event_t {
     unsigned payload_allocated;
 } mcms_event_t;
 
-int main(int argc, char **argv)
+static long snprintf_event(const mcms_event_t *event)
 {
     char buf[BUFSIZ];
-    proctimer_t pt;
-    int elapsed, i, count, len;
-    long nbytes;
-    mcms_event_t ev, *event = &ev;
-    FILE *out1, *out2;
 
-    count = 100000;
-    out1 = out2 = NULL;
-
-    if (argc > 1)
-        count = parse_number(argv[1]);
-    if (argc > 2)
-        out1 = fopen(argv[2], "w");
-    if (argc > 3)
-        out2 = fopen(argv[3], "w");
-
-    p_clear(event, 1);
-    event->stamp = 1178096605;
-
-    nbytes = 0;
-    proctimer_start(&pt);
-
-    for (i = 0; i < count; i++) {
-        event->type = "ABDG"[i & 3];
-        event->msisdn = 33612345678LL + i + (i ^ 4321);
-        event->camp_lineno = i & 16383;
-        event->camp_id = i >> 14;
-        event->remote_id = 1;
-        event->payload_len = 0;
-
-        len = snprintf(buf, sizeof(buf),
-                       "%d|%c|%lld|%d|%d|%u|%d|\n",
-                       event->stamp, event->type,
-                       (long long)event->msisdn,
-                       event->camp_lineno, event->camp_id,
-                       event->remote_id, event->payload_len);
-        nbytes += len;
-        if (out1) {
-            IGNORE(fwrite(buf, 1, len, out1));
-        }
-    }
-
-    elapsed = proctimer_stop(&pt);
-
-    fprintf(stderr, "snprintf: %d tests, %ld bytes, %d.%03d ms.\n",
-            count, nbytes, elapsed / 1000, elapsed % 1000);
-
-    nbytes = 0;
-    proctimer_start(&pt);
-
-    for (i = 0; i < count; i++) {
-        event->type = "ABDG"[i & 3];
-        event->msisdn = 33612345678LL + i + (i ^ 4321);
-        event->camp_lineno = i & 16383;
-        event->camp_id = i >> 14;
-        event->remote_id = 1;
-        event->payload_len = 0;
-
-        len = isnprintf(buf, sizeof(buf),
-                        "%d|%c|%lld|%d|%d|%u|%d|\n",
-                        event->stamp, event->type,
-                        (long long)event->msisdn,
-                        event->camp_lineno, event->camp_id,
-                        event->remote_id, event->payload_len);
-        nbytes += len;
-        if (out2) {
-            IGNORE(fwrite(buf, 1, len, out2));
-        }
-    }
-
-    elapsed = proctimer_stop(&pt);
-
-    fprintf(stderr, "isnprintf: %d tests, %ld bytes, %d.%03d ms.\n",
-            count, nbytes, elapsed / 1000, elapsed % 1000);
-
-    p_fclose(&out1);
-    p_fclose(&out2);
-
-    return 0;
+    return snprintf(buf, sizeof(buf),
+                    "%d|%c|%lld|%d|%d|%u|%d|\n",
+                    event->stamp, event->type,
+                    (long long)event->msisdn,
+                    event->camp_lineno, event->camp_id,
+                    event->remote_id, event->payload_len);
 }
+
+ZBENCH_GROUP_EXPORT(iprintf_bench) {
+    ZBENCH(snprintf) {
+        int i = 0;
+        mcms_event_t ev, *event = &ev;
+
+        p_clear(event, 1);
+        ZBENCH_LOOP() {
+            event->stamp = 1178096605;
+            event->type = "ABDG"[i & 3];
+            event->msisdn = 33612345678LL + i + (i ^ 4321);
+            event->camp_lineno = i & 16383;
+            event->camp_id = i >> 14;
+            event->remote_id = 1;
+            event->payload_len = 0;
+            i++;
+
+            ZBENCH_MEASURE() {
+                for (int j = 0; j < 1000; j++) {
+                    snprintf_event(event);
+                }
+            } ZBENCH_MEASURE_END
+        } ZBENCH_LOOP_END
+    } ZBENCH_END
+} ZBENCH_GROUP_END
