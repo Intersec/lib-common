6ee30c892ba6f241ca428badf2473801f024b2b9
Romain Le Godais <romain.legodais@intersec.com>
2022-02-04 09:30:03 +0100

    Happy new year 2022!
    We are just a bit late this year. Done with the following command:
    
     $ sed -i "s/Copyright 2021/Copyright 2022/g" -- $(git grep -l "Copyright 2021")
    
    Change-Id: I45fdbe1457ad9b71c10a963f95b6cca4c88d8628
    Priv-Id: b1c2f97372922d5e15d364d0315d1f479e4d2bf7

--- tests/pxcc/z_pxcc.py
+++ tests/pxcc/z_pxcc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ###########################################################################
 #                                                                         #
-# Copyright 2021 INTERSEC SA                                              #
+# Copyright 2022 INTERSEC SA                                              #
 #                                                                         #
 # Licensed under the Apache License, Version 2.0 (the "License");         #
 # you may not use this file except in compliance with the License.        #
