1a31f4050bf784ab42a4a8d1bca555f29894f62a
Nicolas Pauss <nicolas.pauss@intersec.com>
2022-05-16 10:14:02 +0200

    mypy: disable type checking on bench/mem_stats.py.
    numpy and matplotlib are not in the poetry environment, we can ignore
    this file.
    
    Change-Id: Iccdde014e37ee0daf43c3f102abdaf5f3af64829
    Refs: #84968
    Priv-Id: 2a6a3d2ea50fa83c0f752302a450805327d03074

--- bench/mem_stats.py
+++ bench/mem_stats.py
@@ -16,6 +16,7 @@
 # limitations under the License.                                          #
 #                                                                         #
 ###########################################################################
+# type: ignore
 
 """ Utility to read mem-bench logs.
 
