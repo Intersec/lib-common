34031c4062e2222e479c2610553e3d07dd3e569f
Romain Le Godais <romain.legodais@intersec.com>
2023-03-27 17:53:54 +0200

    waf: Ensure binaries / shared-libraries are re-linked after configure.
    When changing the build profile (for example changing from debug to
    asan), and then switching back, the binaries were not re-linked because
    nothing changed from the previous run (in the same profile).
    
    So we could easily compile in a profile (let's say asan) but actually
    end up with binaries of the previous build on another profile (for
    example debug), without noticing. This can be a waste of time, and even
    prevent from detecting memory bugs (in that particular case).
    
    Fix that by adding the configure time in the "hcode" of the link tasks,
    which will force a re-link after each configure. The problem with that
    is that the link will be remade even if the profile was actually not
    change, and that there is nothing to do. But I could not find something
    smarter, and this should not be a big problem.
    
    Change-Id: I429f7fd580da6227bff5ff0b030f2eeb1232931e
    Priv-Id: 05fb825c70037aada4267f43f5b97722622ec757

--- build/waftools/backend.py
+++ build/waftools/backend.py
@@ -26,6 +26,7 @@ import re
 import copy
 import shlex
 import os.path as osp
+import time
 from itertools import chain
 
 # pylint: disable = import-error
@@ -282,6 +283,10 @@ def deploy_program(self):
     node = self.link_task.outputs[0]
     self.link_task.outputs = [node.get_src()]
 
+    # Ensure the binaries are re-linked after running configure (in case the
+    # profile was changed)
+    self.link_task.hcode += str(self.env.CONFIGURE_TIME).encode('utf-8')
+
 
 @TaskGen.feature('cshlib')
 @TaskGen.after_method('apply_link')
@@ -296,6 +301,10 @@ def deploy_shlib(self):
         tgt_name = tgt_name[len('lib'):]
     tgt = node.parent.get_src().make_node(tgt_name)
     self.link_task.outputs = [tgt]
+
+    # Ensure the shared libraries are re-linked after running configure (in
+    # case the profile was changed)
+    self.link_task.hcode += str(self.env.CONFIGURE_TIME).encode('utf-8')
 
 
 # }}}
@@ -1663,6 +1672,8 @@ PROFILES = {
 # }}}
 
 def configure(ctx):
+    ctx.env.CONFIGURE_TIME = time.time()
+
     # register_global_includes
     ConfigurationContext.register_global_includes = register_global_includes
 
