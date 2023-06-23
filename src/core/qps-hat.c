98e4b1c96e136877e26e048103c030736b1148b1
Sébastien Thouément <sebastien.thouement@intersec.com>
2023-06-23 11:32:37 +0200

    Revert "qps/hat: undo a change that uncovered a bug yet to investigate"
    This reverts commit 8f545ea59cad36162a353623c1a99a64d7672350.
    
    The commit can be undone now because we identified and fixed the issue
    that it uncovered.
    
    Change-Id: Ic0e70f94d914c4b4c832d71a59b40a17b9293af0
    Priv-Id: 4f70ff6e7d12d11de5fe12cf62cf4f198bc00402

--- src/core/qps-hat.c
+++ src/core/qps-hat.c
@@ -1463,14 +1463,8 @@ qhat_tree_enumerator_get_value_unsafe(const qhat_tree_enumerator_t *en)
         return &qhat_default_zero_g;
     }
 
-    /* FIXME The patch fixing this part has been undone as it
-     * uncovered a bug that caused some QHAT corruptions. It should be
-     * reestablished as soon as the root cause of the corruption is
-     * fixed. */
-#if 0
     /* The caller should probably have used the safe version. */
     assert(qhat_path_is_fully_sync(&en->path));
-#endif
 
     if (en->compact) {
         /* If this assert fails, then it means that returned value isn't the
@@ -1915,13 +1909,9 @@ void qhat_tree_enumerator_find_root(qhat_tree_enumerator_t *en, uint32_t key)
     uint32_t root = qhat_get_key_bits(hat, key, 0);
     ssize_t  i;
 
-    /* FIXME Redmine #94699: this generation update unveil a bug that still
-     * needs to be investigated. */
-#if 0
     /* We're going to refresh the whole path so the structure generation can
      * be updated. */
     en->path.gen = hat->gen;
-#endif
 
     en->path.depth = 0;
     en->path.key   = 0;
