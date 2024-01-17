3a225777f0e8aee8a7e632bcd2e34ba656850080
Nicolas Pauss <nicolas.pauss@intersec.com>
2024-01-17 08:53:20 +0100

    waftools: rework and fix compilation database with clang.
    Multiple rework and fixes in this commit for the compilation database:
    
    1.
    In order to generate the compilation database, we iterate through the
    list of C, C++, Blk and Blkk tasks to retrieve the list of commands used
    to compile the source files.
    Unfortunately, since our standard compiler is GCC and not Clang, for
    plain C and C++ source files, we retrieved commands to compile the
    source files with GCC and not Clang.
    
    The issue is that the JSON compilation database is very closely related
    to Clang: https://clang.llvm.org/docs/JSONCompilationDatabase.html.
    So using the arguments for GCC is kind of wrong and lead to unrecognized
    arguments. A filtering system was already in-place, but it was not
    perfect and not really flexible.
    
    The solution is, when generating the compilation database, to force
    using Clang just for the compilation database instead of GCC.
    This way we are sure that the commands used for the tasks are well
    designed for Clang.
    
    2.
    Fix using 'command' instead of 'arguments' if we have the task command
    as a string and not as a list of arguments.
    This is well specified in the spec:
    https://clang.llvm.org/docs/JSONCompilationDatabase.html
    
    3.
    Improve the code a bit by retrieving the waf task classes only once and
    not when iterating on the task generators.
    
    4.
    Generate compilation commands for headers.
    Instead of only generate the compilation commands for source files,
    also generate them for the headers.
    This way, editors can also provide warnings on header files.
    '-xc-header' and '-xcxx-header' are used for C and C++ headers, while
    '-xc' and '-xcxx' are used for C and C++ sources (useful for .blk and
    .blkk files).
    Unfortunately, due to a bug in clang < 16 with '-fsyntax-only',
    '-Wno-unused' is also required for header types.
    
    5.
    Also add the generated files in the compilation database as it can be
    useful in some cases.
    
    Change-Id: I07782e5d49ebc178d3ef7878f48fa1d9afa5bfe3
    Priv-Id: 10b80e254301b94c291e4078f14fb3baa7bd94d3

--- build/waftools/compilation_database.py
+++ build/waftools/compilation_database.py
@@ -28,54 +28,106 @@ Usage:
 # pylint: disable = import-error
 from waflib import Logs, Task, Build, Scripting
 
-Task.Task.keep_last_cmd = True
+from backend import compute_clang_extra_cflags
+
+
+TASK_CLASSES = None
+
+
+def build_task_classes_map():
+    '''Build the task classes'''
+    global TASK_CLASSES
+
+    assert TASK_CLASSES is None
+    TASK_CLASSES = {
+        x: Task.classes.get(x) for x in
+        ('Iop2c', 'c', 'cxx', 'Blk2c', 'Blkk2cc')
+    }
 
 
 class CompileDbContext(Build.BuildContext):
     '''generates compile_commands.json by request'''
 
-    cmd='compiledb'
+    cmd = 'compiledb'
 
-    def write_one_compilation_db(self, db_file, tasks):
-        database_file=self.srcnode.make_node(db_file)
+    @staticmethod
+    def get_task_cmd(task, is_dep):
+        '''Get the command used for the task'''
+        cmd = task.last_cmd
+
+        if not isinstance(cmd, list):
+            # Add it to list command arguments
+            return cmd
+
+        # Get the file compilation type from the task class
+        additional_args = None
+        if isinstance(task, (TASK_CLASSES['c'], TASK_CLASSES['Blk2c'])):
+            if is_dep:
+                # XXX: Due to a bug with clang with '-fsyntax-only'
+                # on headers, clang still complains about unused symbols even
+                # when specifying the header type.
+                additional_args = ['-xc-header', '-Wno-unused']
+            else:
+                additional_args = ['-xc']
+        elif isinstance(task, (TASK_CLASSES['cxx'], TASK_CLASSES['Blkk2cc'])):
+            if is_dep:
+                # XXX: Due to a bug with clang with '-fsyntax-only'
+                # on headers, clang still complains about unused symbols even
+                # when specifying the header type.
+                additional_args = ['-xcxx-header', '-Wno-unused']
+            else:
+                additional_args = ['-xcxx']
+
+        if additional_args is not None:
+            # Insert additional arguments.
+            cmd = cmd + additional_args
+
+        return cmd
+
+    def add_task_nodes_db(self, clang_db, task, cmd, f_nodes):
+        '''Add the nodes to the db'''
+        for f_node in f_nodes:
+            filename = f_node.path_from(self.srcnode)
+            if filename in clang_db:
+                # Only record the first compilation
+                continue
+
+            entry = {
+                "directory": task.get_cwd().abspath(),
+                "file": filename,
+            }
+
+            # Add the command as 'arguments' if the cmd is a list,
+            # otherwise, use 'command', as specified in compilation database
+            # specification:
+            # https://clang.llvm.org/docs/JSONCompilationDatabase.html
+            if isinstance(cmd, list):
+                entry['arguments'] = cmd
+            else:
+                assert isinstance(cmd, str)
+                entry['command'] = cmd
+
+            clang_db[filename] = entry
+
+    def write_one_compilation_db(self, db_file, tasks,
+                                 dep_additional_args=None):
+        database_file = self.srcnode.make_node(db_file)
         Logs.info('Build commands will be stored in %s',
                   database_file.path_from(self.path))
 
-        try:
-            root = database_file.read_json()
-        except IOError:
-            root = []
-        clang_db = dict((x['file'], x) for x in root)
+        empty_list = []
+        clang_db = {}
 
-        # Those arguments are not recognized by clangd. They will be removed.
-        clang_args_to_filter = ['-cc1', '-internal-isystem',
-                                '-internal-externc-isystem']
         for task in tasks:
-            try:
-                if isinstance(task.last_cmd, list):
-                    cmd = []
-                    for arg in task.last_cmd:
-                        # Blocks handling seems to require a different option
-                        # for clangd.
-                        if arg == '-rewrite-blocks':
-                            cmd.append('-fblocks')
-                        elif arg in clang_args_to_filter:
-                            continue
-                        else:
-                            cmd.append(arg)
-                else:
-                    cmd = task.last_cmd
-            except AttributeError:
-                continue
+            # Add the task node to the db
+            task_node = task.inputs[0]
+            cmd = self.get_task_cmd(task, False)
+            self.add_task_nodes_db(clang_db, task, cmd, [task_node])
 
-            f_node = task.inputs[0]
-            filename = f_node.path_from(self.srcnode)
-            entry = {
-                "directory": task.get_cwd().abspath(),
-                "arguments": cmd,
-                "file": filename,
-            }
-            clang_db[filename] = entry
+            # Add the dependencies of the task
+            dep_nodes = self.node_deps.get(task.uid(), empty_list)
+            cmd = self.get_task_cmd(task, True)
+            self.add_task_nodes_db(clang_db, task, cmd, dep_nodes)
 
         root = list(clang_db.values())
         database_file.write_json(root)
@@ -98,12 +150,42 @@ class CompileDbContext(Build.BuildContext):
         if not self.all_envs:
             self.load_envs()
 
+        # Force clang environment
+        self.env.DO_CHECK = False
+
+        if self.env.COMPILER_CC != 'clang':
+            self.env.COMPILER_CC = 'clang'
+            self.env.CC = self.env.CLANG
+            extra_cflags = compute_clang_extra_cflags(
+                self, self.env.CLANG_FLAGS, 'CFLAGS')
+            include_cflags = ['-I' + include for include in
+                              self.env.INCLUDES_clang]
+            self.env.CFLAGS = (
+                self.env.CLANG_FLAGS + extra_cflags + include_cflags
+            )
+
+        if self.env.COMPILER_CC != 'clang++':
+            self.env.COMPILER_CXX = 'clang++'
+            self.env.CXX = self.env.CLANGXX
+            extra_cxxflags = compute_clang_extra_cflags(
+                self, self.env.CLANGXX_FLAGS, 'CXXFLAGS')
+            include_cxxflags = ['-I' + include for include in
+                                self.env.INCLUDES_clang_cpp]
+            self.env.CXXFLAGS = (
+                self.env.CLANGXX_FLAGS + extra_cxxflags + include_cxxflags
+            )
+
         self.recurse([self.run_dir])
 
         # we need only to generate last_cmd, so override
         # exec_command temporarily
         def exec_command(self, *k, **kw):
             return 0
+
+        # Get the list of classes to filter
+        build_task_classes_map()
+        classes = tuple(TASK_CLASSES.values())
+        iop2c = TASK_CLASSES['Iop2c']
 
         for g in self.groups:
             for tg in g:
@@ -119,14 +201,8 @@ class CompileDbContext(Build.BuildContext):
                 else:
                     lst = tg.tasks
 
-                classes = [Task.classes.get(x) for x in ('c', 'cxx', 'Blk2c',
-                                                         'Blkk2cc')]
-                iop2c = Task.classes.get('Iop2c')
-                classes.append(iop2c)
-                tup = tuple(y for y in classes if y)
                 for tsk in lst:
-                    if (not isinstance(tsk, tup) or
-                            tsk.inputs[0] in self.env.GEN_FILES):
+                    if not isinstance(tsk, classes):
                         continue
 
                     if isinstance(tsk, iop2c):
@@ -134,6 +210,7 @@ class CompileDbContext(Build.BuildContext):
                     else:
                         self.clang_compilation_database_tasks.append(tsk)
                     tsk.nocache = True
+                    tsk.keep_last_cmd = True
                     old_exec = tsk.exec_command
                     tsk.exec_command = exec_command
                     tsk.run()
