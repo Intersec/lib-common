b81488b3923c3f51bd50d580aeedea6ba5d6d017
Nicolas Pauss <nicolas.pauss@intersec.com>
2025-08-05 10:49:34 +0200

    tests/iop-http: add HTTP headers tests for IOP RPC HTTP.
    Test that we can inject and receive HTTP headers from both the client
    and servers with IOP RPC HTTP.
    
    Change-Id: I3a302c045e8f18b0677229ae2ba991687f2d906e
    Priv-Id: a49a8711b50075bb6686593d95d8c7de8423a802

--- tests/zchk-iop-http.c
+++ tests/zchk-iop-http.c
@@ -43,6 +43,9 @@ static struct {
     ic_status_t query_status;
     http_code_t query_code;
 
+    bool query_http_header;
+    bool resp_http_header;
+
     /* for el_wait_until */
     bool el_wait_timed_out;
 } z_iop_http_g;
@@ -69,16 +72,42 @@ static void z_iop_http_el_wait(el_t ev, data_t data)
 
 /* {{{ Tests */
 
-static void z_http_iop_on_connection_error(
+static void z_iop_httpc_on_connection_error(
     http_iop_channel_remote_t *remote, int errnum)
 {
     _G.query_conn_error = errnum;
 }
 
-static void z_http_iop_on_ready(http_iop_channel_t *channel)
+static void z_iop_httpc_on_ready(http_iop_channel_t *channel)
 {
     /* Never actually called in our case because for HTT1X httpc connection is
      * used right away and never turns ready. */
+}
+
+static int z_iop_httpd_on_query_done(
+    const httpd_trigger__ic_t * nonnull tcb, ichttp_query_t * nonnull iq)
+{
+    const http_qhdr_t *qhdr;
+
+    qhdr = http_qhdr_find_from_key(iq->qinfo->hdrs, iq->qinfo->hdrs_len,
+                                   LSTR("X-ZCHK-IOP-HTTP-QUERY"));
+    if (!qhdr || !lstr_equal(LSTR_PS_V(&qhdr->val), LSTR("1"))) {
+        httpd_reject(&iq->super, BAD_REQUEST,
+                     "Missing X-ZCHK-IOP-HTTP-QUERY header");
+        return -1;
+    }
+
+    _G.query_http_header = true;
+    return 0;
+}
+
+static void z_iop_httpd_on_reply_http_headers(
+    const httpd_trigger__ic_t * nonnull tcb,
+    ichttp_query_t * nonnull iq, http_code_t res_code)
+{
+    outbuf_t *ob = httpd_get_ob(&iq->super);
+
+    ob_adds(ob, "X-ZCHK-IOP-HTTP-RESP: 99\r\n");
 }
 
 static void z_iop_httpd_pre_hook(ichannel_t * nullable channel, uint64_t slot,
@@ -127,13 +156,22 @@ static void IOP_RPC_IMPL(tstiop__t, iface, f)
 
 static void IOP_HTTP_RPC_CB(tstiop__t, iface, f)
 {
-    if (status == IC_MSG_OK) {
-        _G.val_answered = res->i;
-    }
-
     _G.query_answered = true;
     _G.query_status = status;
     _G.query_code = OPT_DEFVAL(http_code, 0);
+
+    if (status == IC_MSG_OK) {
+        const httpc_qinfo_t *qinfo = msg->query.qinfo;
+        const http_qhdr_t *qhdr;
+
+        _G.val_answered = res->i;
+
+        qhdr = http_qhdr_find_from_key(qinfo->hdrs, qinfo->hdrs_len,
+                                       LSTR("X-ZCHK-IOP-HTTP-RESP"));
+        _G.resp_http_header = (
+            qhdr && lstr_equal(LSTR_PS_V(&qhdr->val), LSTR("99"))
+        );
+    }
 }
 
 #define SCHEMA  "http://example.com/tstiop"
@@ -151,6 +189,8 @@ static int z_iop_http_create_server(const iop_env_t *iop_env,
 
     _G.itcb = httpd_trigger__ic_new(iop_env, &tstiop__t__mod, SCHEMA, 2 << 20);
     _G.itcb->query_max_size = 2 << 20;
+    _G.itcb->on_query_done = &z_iop_httpd_on_query_done;
+    _G.itcb->on_reply_http_headers = &z_iop_httpd_on_reply_http_headers;
     httpd_trigger_register(cfg, POST, "iop", &_G.itcb->cb);
     ichttp_register_pre_post_hook(_G.itcb, tstiop__t, iface, f,
                                   &z_iop_httpd_pre_hook,
@@ -198,8 +238,8 @@ static int z_iop_http_create_client(const iop_env_t *iop_env,
         .max_connections = OPT(1),
         .connection_timeout_msec = OPT(HTTP_TEST_NOACT_DELAY),
         .response_max_size = OPT(2 << 20),
-        .on_connection_error_cb = &z_http_iop_on_connection_error,
-        .on_ready_cb = &z_http_iop_on_ready,
+        .on_connection_error_cb = &z_iop_httpc_on_connection_error,
+        .on_ready_cb = &z_iop_httpc_on_ready,
     };
 
     _G.client_channel = http_iop_channel_create(&channel_cfg, &err);
@@ -237,8 +277,13 @@ static void z_iop_http_query(int i)
     _G.query_answered = false;
     _G.query_status = 0;
     _G.query_code = 0;
+    _G.query_http_header = false;
+    _G.resp_http_header = false;
 
     msg = http_iop_msg_new(0);
+    msg->http_headers = lstr_dup(LSTR(
+        "X-ZCHK-IOP-HTTP-QUERY: 1\r\n"
+    ));
     http_iop_query(_G.client_channel, msg, tstiop__t, iface, f,
                    .i = i);
 
@@ -284,6 +329,8 @@ z_iop_http_do_simple_query(const iop_env_t *iop_env,
         Z_ASSERT_EQ(_G.query_conn_error, 0);
         Z_ASSERT_EQ(_G.query_status, (unsigned)IC_MSG_OK);
         Z_ASSERT_EQ(_G.query_code, (unsigned)HTTP_CODE_OK);
+        Z_ASSERT(_G.query_http_header);
+        Z_ASSERT(_G.resp_http_header);
     }
 
     Z_HELPER_RUN(z_iop_http_finalize());
