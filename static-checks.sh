d2c463bb3b084e680e927b1c03c0d5bb615216e5
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-10-02 17:03:16 +0200

    static-checks: add "staged_files" to only check staged files
    It will be useful to have a git hook use static-checks.sh.
    My code contains a comment dedicated to shellcheck, I know it's not yet
    commonly used in Intersec but it's the best tool to check shell/bash so
    I use it and I hope other people will use it too.
    --force-exclude to make it respect the excluded files of ruff.
    
    Change-Id: I8c215a0fe027a5576fe1aadd78ade495747d6945
    Priv-Id: ce158e5a1ff6c542b0f6bf290d242cb04d283e18

--- static-checks.sh
+++ static-checks.sh
@@ -17,18 +17,93 @@
 #                                                                         #
 ###########################################################################
 
-set -eu
+set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+USAGE="$(cat <<EOM
+Usage: $0 [-h | --help] [--staged-files]
+    -h | --help:    Print this help
+    --staged-files: Process only the staged files
+EOM
+)"
+
 
 run_cmd() {
     echo "---" "$@" "---"
     "$@"
 }
 
+modified_python_files() {
+    local diff_mode="$1"
+    local git_diff_files
+
+    if [ "$diff_mode" = "staged-files" ]; then
+        # Get only the staged files
+        git_diff_files="git diff-index --name-only --cached HEAD"
+    else
+        # By default, we check the last commit
+        git_diff_files="git diff --name-only HEAD^"
+    fi
+
+    # Filter only the existing files and not the removed ones
+    # Convert new lines into spaces for nice printing
+    eval "$git_diff_files -- \
+            '*.py' '**/*.py' '*.pyi' '**/*.pyi' 'wscript*' '**/wscript*' | \
+            xargs --no-run-if-empty stat --printf '%n ' 2>/dev/null"
+}
+
+run_on_modified_python_files() {
+    local cmd="$1"
+    local modified_files="$2"
+
+    # Run each command in parallel with xargs
+    echo "$modified_files" | \
+        xargs --no-run-if-empty -n 1 -P "$(nproc --all)" "$cmd"
+}
+
 main() {
+    local params
+    local modified_files
+    local diff_mode="last-commit"
+    local supported_commands="help,staged-files"
+
     # Go to the root directory of the repository
     cd "$SCRIPT_DIR"
+
+    # Parse options
+    params="$(getopt -o h -l "$supported_commands" --name "$0" -- "$@")"
+    eval set -- "$params"
+
+    while true; do
+        case "$1" in
+        -h|--help)
+            echo "$USAGE"
+            exit 0
+            ;;
+        --staged-files)
+            diff_mode="staged-files"
+            shift
+            ;;
+        --)
+            # End of arguments
+            shift
+            break
+            ;;
+        *)
+            echo "Not implemented: $1" >&2
+            echo "$USAGE" >&2
+            exit 1
+            ;;
+        esac
+    done
+
+    modified_files="$(modified_python_files "$diff_mode")"
+
+    # If no python files are staged, do not run any linting tool
+    if [[ -z "$modified_files" ]] && [[ "$diff_mode" = "staged-files" ]]; then
+        return 0
+    fi
 
     # Run the different commands
     run_cmd waf ruff
@@ -37,4 +112,4 @@ main() {
     run_cmd cargo fmt --check
 }
 
-main
+main "$@"
