a8802f050ec56c34feeca6b7e0eb87ead3157852
Nicolas Pauss <nicolas.pauss@intersec.com>
2025-12-05 09:36:14 +0100

    rust/thr: add main_c_queue_schedule() to run closures in main C thread.
    It is necessary to be able to run C functions that required to be run in
    the main C thread like IC queries or replies.
    
    Change-Id: I2a44dfa30675af08a7f7c0bbb53b47c97c798f0c
    Refs: #113515
    Priv-Id: f20b368c78a16a4b99b2b474c51163db63b0966c

--- rust/libcommon-core/thr.rs
+++ rust/libcommon-core/thr.rs
@@ -19,19 +19,75 @@
 //! Module to export the thread functions used by `libcommon` in a safe way
 //!
 //! Using `attach()` and `detach()` are required to use `TScope`.
+//!
+//! `main_c_queue_schedule()` can be used to run a closure into the main C thread.
 
-use crate::bindings;
+use std::os::raw::c_void;
+
+use crate::bindings::{
+    module_is_loaded, thr_attach, thr_detach, thr_get_module, thr_is_on_queue,
+    thr_main_queue_schedule, thr_queue_main_g,
+};
+
+// {{{ Attach/Detach
 
 /// Attach the thread to `libcommon` and call the attach callbacks.
 pub fn attach() {
     unsafe {
-        bindings::thr_attach();
+        thr_attach();
     };
 }
 
 /// Detach the thread from `libcommon` and call the detach callbacks.
 pub fn detach() {
     unsafe {
-        bindings::thr_detach();
+        thr_detach();
     };
 }
+
+// }}}
+// {{{ Run main thread
+
+/// Trampoline function to call the closure from the main C thread.
+unsafe extern "C" fn main_c_thread_call_closure<F>(data: *mut c_void)
+where
+    F: FnOnce() + Send + 'static,
+{
+    let callback_ptr = data.cast::<F>();
+    let callback = unsafe { Box::from_raw(callback_ptr) };
+    callback();
+}
+
+/// Schedule a closure to be run on the main C thread.
+///
+/// # Example
+///
+/// ```rust
+/// main_c_queue_schedule(move || {
+///     unsafe {
+///         ic_reply(...);
+///     }
+/// });
+/// ```
+pub fn main_c_queue_schedule<F>(callback: F)
+where
+    F: FnOnce() + Send + 'static,
+{
+    // Assert that the `thr` module is loaded.
+    debug_assert!(unsafe { module_is_loaded(thr_get_module()) });
+
+    if unsafe { thr_is_on_queue(thr_queue_main_g) } {
+        // We are already on the main C thread, run the callback immediately.
+        callback();
+        return;
+    }
+
+    // Else, schedule the callback on the main thread.
+    let data = Box::into_raw(Box::new(callback));
+    unsafe {
+        // Bindgen is weird and expects an Option here.
+        thr_main_queue_schedule(Some(main_c_thread_call_closure::<F>), data.cast());
+    }
+}
+
+// }}}
